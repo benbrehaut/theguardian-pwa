a37b02ac45a0583633d8aef00a946d1552ca58d6
Ben Brehaut <benbrehaut@redweb.com>
2019-12-11 17:05:40 +0000

    Adding backdrop for menu

--- src/Components/AppMenuTriggerButton/AppMenuTriggerButton.react.js
+++ src/Components/AppMenuTriggerButton/AppMenuTriggerButton.react.js
@@ -9,18 +9,20 @@ import './AppMenuTriggerButton.scss'
 export default function AppMenuTriggerButton() {
   const state = useContext(MenuContext)
 
-  function changeMenuState() {
+  const changeMenuState = () => {
     state.menuOpen ? state.setMenu(false) : state.setMenu(true)
   }
 
   return(
     <>
-      <button className={`o-app-menu-toggler ${state.menuOpen ? 'is-open' : ''}`} type="button" onClick={() => changeMenuState()}>
+      <button id="menu-trigger" className={`o-app-menu-toggler ${state.menuOpen ? 'is-open' : ''}`} type="button" onClick={() => changeMenuState()}>
         <ScreenReaderText>Toggle Menu</ScreenReaderText>
         <span className="o-app-menu-toggler__line o-app-menu-toggler__line--top" aria-hidden="true"></span>
         <span className="o-app-menu-toggler__line o-app-menu-toggler__line--middle" aria-hidden="true"></span>
         <span className="o-app-menu-toggler__line o-app-menu-toggler__line--bottom" aria-hidden="true"></span>
       </button>
+
+      <label htmlFor="menu-trigger" className={`o-app-menu-overlay ${state.menuOpen ? 'is-active' : ''}`}></label>
     </>
   )
 }
