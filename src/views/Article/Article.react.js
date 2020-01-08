23c4aa5081846afabf1c6b0c5d9f58d3a990f5df
Ben Brehaut <bencbrehaut@gmail.com>
2020-01-08 23:01:02 +0000

    addign actions for article appbar

--- src/views/Article/Article.react.js
+++ src/views/Article/Article.react.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
-import AppBar from './../../Components/AppBar'
-
 import api from '../../services/api.js'
-
+import AppBar from './../../Components/AppBar'
+import ActionBadge from '../../Components/Common/ActionBadge'
+import { ReactComponent as BackArrowIcon } from './../../Static/icons/chevron-left.svg';
+import { ReactComponent as ShareIcon } from './../../Static/icons/share.svg';
 
 export default class Article extends Component {
   constructor(props) {
@@ -13,6 +13,9 @@ export default class Article extends Component {
       article: [],
       isLoading: true
     }
+
+    this.shareArticle = this.shareArticle.bind(this);
+    this.goBack = this.goBack.bind(this);
   }
 
   componentDidMount() {
@@ -32,23 +35,65 @@ export default class Article extends Component {
       })
   }
 
+  /**
+   * goBack
+   * Check how the user has come to the article and either go back to the previous app page
+   * or take them to the homepage
+   */
+  goBack = () => {
+    const {history} = this.props;
+
+    // if user has come directly to the article
+    if (history.action === "POP") {
+      history.push('/')
+    }
+    // else they have come from isde the app
+    else {
+      history.goBack()
+    }
+  }
+  
+  // TODO
+  shareArticle() {
+    const $this = this;
+
+    if (navigator.share !== undefined) {
+      navigator.share({
+        title: 'Guardian PWA Article',
+        text: 'Check out this article',
+        url: window.location.href
+      })
+    }
+    else {
+      console.log($this)
+    }
+  }
+
   render() {
     const { article, isLoading } = this.state;
     return(
       <>
-        <AppBar>
-          Article
-        </AppBar>
+        <AppBar title="Article" 
+          leftIcon={
+            <ActionBadge fill="#C70000" onClick={this.goBack} text="Return to previous page">
+              <BackArrowIcon />
+            </ActionBadge>
+          }
+          rightIcon={
+            <ActionBadge fill="#C70000" onClick={this.shareArticle} text="Share article">
+              <ShareIcon />
+            </ActionBadge>
+          }
+        />
         {!isLoading ? (
-          <div>
-            { article.sectionName }
+          <article className="c-article" role="article">
             <img src={article.fields.thumbnail} alt={ article.fields.byline } />
             <h1>{ article.webTitle }</h1>
             <p dangerouslySetInnerHTML={{ __html: article.fields.standfirst }} />
             <time>{ article.fields.firstPublicationDate }</time>
             { article.fields.byline }
             <div dangerouslySetInnerHTML={{ __html: article.fields.body }} />
-          </div>
+          </article>
         ) : (
           <div>
             <p>Is loading</p>
